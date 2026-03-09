9179fff1952265ec074a60851dad5c16d6168052
Akash <akashsubramani2002@gmail.com>
2026-03-09 11:34:14 +0530

    praticing-javascript

---
+++ practicing-code/level-1/largest-array.js
@@ -0,0 +1,10 @@
+function largest(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(largest([2, 3, 4, 8, 7]));
